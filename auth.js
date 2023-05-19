1764878c916c72697baa9b68addd3528c3f441b3
ernas93 <107477572+ernas93@users.noreply.github.com>
2023-05-19 19:10:17 +0200

    typo

--- auth.js
+++ auth.js
@@ -19,7 +19,7 @@ module.exports = (router) => {
         passport.authenticate('local', { session: false }, (error, user, info) => {
             if (error || !user) {
                 return res.status(400).json({
-                    message: 'Something went worng.',
+                    message: 'Something went wrong.',
                     user: user 
                 });
             }
